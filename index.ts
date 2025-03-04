4529fef52ab6a5273b63ee46140661d100ce79bc
Valentinskiq <v.valentinov2021@yahoo.com>
2025-03-04 15:20:56 +0200

    changes in index.file TypeScript first lec

--- index.ts
+++ index.ts
@@ -2,40 +2,34 @@
 let number: number = 5;
 number = 10;
 
+const message: string = "Welcome";
 
-//const - ne mogat da budat prezapisvani za razlika ot let
-const message: string = 'Welcome'
-
-//Komentar String
-let welcomeMessage: string = "Hello, all!";
+// String
+let welcomeMessage: string = "Hello, Peter!";
 let test: string = "Ivan";
 
-let welcomeMessageLenght: number = welcomeMessage.length;
+let welcomeMessageLength: number = welcomeMessage.length;
 
+// String interpolation
+let welcomeMessageDetails: string = `My welcome message length is ${welcomeMessageLength}`;
+let userWelcomeMessage: string = `Welcome, ${test}`;
 
-//string interpolation
-let  welcomeMessageDetails: string = 'My welocme message length is ${welcomeMessageLength}';
-let userWelocmeMessage: string = 'Welcome, ${test}';
-
-
-//Number
-
+// Number
 let firstNumber: number = 5;
 let secondNumber: number = 10;
 
 //Number math operations
-
 let add: number = secondNumber + firstNumber;
 let substract: number = secondNumber - firstNumber;
 let multiply: number = secondNumber * firstNumber;
 let divide: number = secondNumber / firstNumber;
 
-//Boolean (true/false)
-
+// Boolean (true/false)
 let isGreater: boolean = firstNumber > secondNumber;
 let isLower: boolean = firstNumber < secondNumber;
-let  isEqual: boolean = firstNumber === secondNumber;
+let isEqual: boolean = firstNumber === secondNumber;
 let isGreaterOrEqual: boolean = firstNumber >= secondNumber;
-let isLowerOrEqual: boolean = firstNumber  <= secondNumber;
+let isLowerOrEqual: boolean = firstNumber <= secondNumber;
 
-console.log("isGreaterOrEqual", isGreaterOrEqual);
+let areNumbersEqualTo: boolean = 5 === firstNumber && 10 === secondNumber;
+let isAnyNumberEqualTo: boolean = 6 === firstNumber || 11 === secondNumber;
